fb2030f8ec4f873c4aed1033701697ac3dd4c761
Maurisz <p2002adam@gmail.com>
2022-10-11 08:29:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function fuggveny (param1,param2){
+    console.log(param1);
+}
+
+let masikFv = function (param1,param2){
+    console.log(param2);
+}
+
+//let init = function (){
+  //  console.log('Page loaded')
+//}
+
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('Page loaded')
+});
