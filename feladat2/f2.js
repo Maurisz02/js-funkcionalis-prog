0e766b02977d999cb54d5dacb659055078fc9444
Maurisz <p2002adam@gmail.com>
2022-10-11 11:55:58 +0200

    4-5 feladat

--- feladat2/f2.js
+++ feladat2/f2.js
@@ -1,6 +1,33 @@
 
 let tomb = [];
 
+class Szazlabu{
+    #labakSzama;
+
+    constructor(labakSzama){
+        this.#labakSzama = labakSzama;
+    }
+
+    kiir(){
+        console.log(this.#labakSzama+" lábú százlábú");
+    }
+
+};
+
+function labakbolSzazlabuk(labakTomb){
+
+    let szazLabu = []
+    labakTomb.forEach((e,index)=>{
+        let sz = new Szazlabu(e);
+        szazLabu.push(sz);
+    });
+
+    szazLabu.forEach((e)=>{
+        e.kiir();
+    })
+
+};
+
 document.addEventListener("DOMContentLoaded",()=>{
     for(let i = 0; i < 20; i++){
         let rnd = Math.floor(Math.random() * 29)+1;
@@ -29,5 +56,7 @@ document.addEventListener("DOMContentLoaded",()=>{
         tomb.push(num);
         console.log(tomb);
      });
+
+     labakbolSzazlabuk(tomb);
 
 });
