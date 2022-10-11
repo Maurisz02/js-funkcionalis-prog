ee0144dc0e913e2553adac163b54c0b031f4a9a7
Maurisz <p2002adam@gmail.com>
2022-10-11 09:03:17 +0200

    set intervals

--- index.js
+++ index.js
@@ -12,6 +12,36 @@ let masikFv = function (param1,param2){
   //  console.log('Page loaded')
 //}
 
+/*
 document.addEventListener('DOMContentLoaded',function(){
     console.log('Page loaded')
+
+    document.getElementById('szinez').addEventListener('click', function(){
+        document.body.style.backgroundColor = 'lightblue';
+    });
+});*/
+
+// ====
+document.addEventListener('DOMContentLoaded',() => {
+    console.log('Page loaded');
+
+    let idozito;
+
+    document.getElementById('szinez').addEventListener('click',()=>{
+        document.body.style.backgroundColor = 'lightblue';
+
+        setTimeout(()=>{
+            document.body.style.backgroundColor = 'black';
+        },2000);
+
+        
+        if(idozito !== undefined){
+            clearInterval(idozito);
+        }
+
+        idozito = setInterval(()=>{
+            console.log(document.body.style.backgroundColor);
+        },1000);
+
+    });
 });
