84d7ec4dce31b8caea54aaf872921b8d01fd5aba
Maurisz <p2002adam@gmail.com>
2022-10-11 09:18:45 +0200

    filter,sort

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ let masikFv = function (param1,param2){
     console.log(param2);
 }
 
+
+function osszeHasonlit(a,b){
+    if(a < b){
+        return -1;
+    }else if(a > b){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+let t = [4, 78, 71, -99];
+//t.sort(osszeHasonlit);
+t.sort((a,b)=>{
+    if(a < b){
+        return -1;
+    }else if(a > b){
+        return 1;
+    }else{
+        return 0;
+    }
+});
+console.log(t);
+
+t.sort((a,b)=>{return a - b});
+
+t = t.filter((e)=>{return e > 0;});
+
+console.log(t);
+
+
+
+
+
+
+
 //let init = function (){
   //  console.log('Page loaded')
 //}
@@ -34,7 +70,7 @@ document.addEventListener('DOMContentLoaded',() => {
             document.body.style.backgroundColor = 'black';
         },2000);
 
-        
+
         if(idozito !== undefined){
             clearInterval(idozito);
         }
